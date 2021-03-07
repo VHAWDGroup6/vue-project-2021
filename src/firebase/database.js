27be1131b100d4b13f31038b68dbb8053d0189ae
Vikki Hanley <vh.awdgroup6@gmail.com>
2021-03-07 22:44:17 +0000

    Config changes

--- src/firebase/database.js
+++ src/firebase/database.js
@@ -4,14 +4,14 @@ import "firebase/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBWx4p0j16mdUIivZLBqz_8l_BH6OhRpR4",
-    authDomain: "myocardio-awdg6.firebaseapp.com",
-    projectId: "myocardio-awdg6",
-    storageBucket: "myocardio-awdg6.appspot.com",
-    messagingSenderId: "886784373926",
-    appId: "1:886784373926:web:08305df28d2701e4c3b067",
-    measurementId: "G-PK42GS9GFN"
-  };
+  apiKey: "AIzaSyDk-mvZ_FepmBg9t91muRkASWuIXlnvhMM",
+  authDomain: "awd-vh.firebaseapp.com",
+  projectId: "awd-vh",
+  storageBucket: "awd-vh.appspot.com",
+  messagingSenderId: "592262378665",
+  appId: "1:592262378665:web:62e8d1363198f3449ea5b8",
+  measurementId: "G-NBLMKGEH87"
+};
 
   //initialize firebase
   firebase.initializeApp(firebaseConfig);
